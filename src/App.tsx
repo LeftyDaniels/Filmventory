0d109cfe18de698736358e1c4a26a07442458ed5
LeftyDaniels <35117222+LeftyDaniels@users.noreply.github.com>
2020-02-16 01:21:28 -0600

    Initialize Project
    Initilize the React project and add a few things like Material-UI, Prettier and some other stuff.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
+
+function App() {
+  return (
+    <ThemeProvider theme>
+      <div />
+    </ThemeProvider>
+  );
+}
+
+export default App;
